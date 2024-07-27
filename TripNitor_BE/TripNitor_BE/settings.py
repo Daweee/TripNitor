e76779a4f9807e72d151edd47867acf53de5d84b
Daweee <49843513+Daweee@users.noreply.github.com>
2024-07-27 16:40:03 +0800

    added additional backend setups

--- TripNitor_BE/TripNitor_BE/settings.py
+++ TripNitor_BE/TripNitor_BE/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TN_Api',
-    
+    'drf_spectacular',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'TripNitor API',
+}
