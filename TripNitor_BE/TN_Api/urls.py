9e983f2f2eca055c0ee1b1cff581a90fbf089f23
Daweee <49843513+Daweee@users.noreply.github.com>
2024-08-25 03:37:13 +0800

    fixed url pattern

--- TripNitor_BE/TN_Api/urls.py
+++ TripNitor_BE/TN_Api/urls.py
@@ -13,19 +13,19 @@ urlpatterns = [
 
     path('drivers/register/', DriverCreateView.as_view(), name='register_driver'),
     path('drivers/', DriverListView.as_view(), name='driver_list'),
-    path('drivers/<int:pk>/', DriverDetailView.as_view(), name='get_driver'),
-    path('drivers/<int:pk>/update/', DriverUpdateView.as_view(), name='update_driver'),
-    path('drivers/<int:pk>/delete/', DriverDeleteView.as_view(), name='delete_driver'),
+    path('drivers/<str:pk>/', DriverDetailView.as_view(), name='get_driver'),
+    path('drivers/<str:pk>/update/', DriverUpdateView.as_view(), name='update_driver'),
+    path('drivers/<str:pk>/delete/', DriverDeleteView.as_view(), name='delete_driver'),
 
     path('vans/register/', VanCreateView.as_view(), name='register_van'),
     path('vans/', VanListView.as_view(), name='van_list'),
-    path('vans/<int:pk>/', VanDetailView.as_view(), name='get_van'),
-    path('vans/<int:pk>/update/', VanUpdateView.as_view(), name='update_van'),
-    path('vans/<int:pk>/delete', VanDeleteView.as_view(), name='delete_van'),
+    path('vans/<str:pk>/', VanDetailView.as_view(), name='get_van'),
+    path('vans/<str:pk>/update/', VanUpdateView.as_view(), name='update_van'),
+    path('vans/<str:pk>/delete', VanDeleteView.as_view(), name='delete_van'),
 
     path('gas/register/', GasCreateView.as_view(), name='register_gas'),
     path('gas/', GasListView.as_view(), name='van_gas'),
-    path('gas/<int:pk>/', GasDetailView.as_view(), name='get_gas'),
-    path('gas/<int:pk>/update/', GasUpdateView.as_view(), name='update_gas'),
-    path('gas/<int:pk>/delete', GasDeleteView.as_view(), name='delete_gas'),
+    path('gas/<str:pk>/', GasDetailView.as_view(), name='get_gas'),
+    path('gas/<str:pk>/update/', GasUpdateView.as_view(), name='update_gas'),
+    path('gas/<str:pk>/delete', GasDeleteView.as_view(), name='delete_gas'),
 ]
