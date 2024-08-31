e74e2fa83280ad1cbdd2223dd7950a93741e64e2
Daweee <49843513+Daweee@users.noreply.github.com>
2024-09-01 02:04:16 +0800

    modified token service

--- tripnitor_mobile_app/lib/services/token_service.dart
+++ tripnitor_mobile_app/lib/services/token_service.dart
@@ -3,7 +3,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class TokenService {
   final FlutterSecureStorage _storage = FlutterSecureStorage();
 
-  Future<void> saveTokens(String accessToken, String refreshToken) async {
+  Future<void> saveTokens(String id, String accessToken, String refreshToken) async {
+    await _storage.write(key: 'id', value: id);
     await _storage.write(key: 'access_token', value: accessToken);
     await _storage.write(key: 'refresh_token', value: refreshToken);
   }
@@ -16,7 +17,12 @@ class TokenService {
     return await _storage.read(key: 'refresh_token');
   }
 
+  Future<String?> getUserId() async {
+    return await _storage.read(key: 'id');
+  }
+
   Future<void> deleteTokens() async {
+    await _storage.delete(key: 'id');
     await _storage.delete(key: 'access_token');
     await _storage.delete(key: 'refresh_token');
   }
