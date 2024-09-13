1d6d6a36f2166ce8d232b3755696e6899554a3bb
Daweee <49843513+Daweee@users.noreply.github.com>
2024-09-14 00:37:34 +0800

    added respective url and initiatializations

--- TripNitor_BE/TN_Api/urls.py
+++ TripNitor_BE/TN_Api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import (
-    RegisterView, LoginView, LogoutView, 
+    RegisterView, LoginView, LogoutView, UserDetailView, 
     DriverCreateView,DriverListView,DriverDetailView,DriverUpdateView,DriverDeleteView,
     VanCreateView, VanListView, VanDetailView, VanUpdateView, VanDeleteView,
     GasCreateView, GasListView, GasDetailView, GasUpdateView, GasDeleteView,
-    BookingCreateView, BookingListView, BookingDetailView, BookingUpdateView, BookingDeleteView, BookingPreviewView,
+    BookingCreateView, BookingListView, BookingDetailView, BookingUpdateView, BookingDeleteView, BookingPreviewView, UserBookingListView, GetBookingStatusListView,
     PackageCreateView, PackageListView, PackageDetailView, PackageUpdateView, PackageDeleteView
 )
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('users/register/', RegisterView.as_view(), name='register'),
     path('users/login/', LoginView.as_view(), name='login'),
     path('users/logout/', LogoutView.as_view(), name='logout'),
+    path('users/<str:pk>/', UserDetailView.as_view(), name='get_user'),
 
     path('drivers/register/', DriverCreateView.as_view(), name='register_driver'),
     path('drivers/', DriverListView.as_view(), name='driver_list'),
@@ -31,6 +32,8 @@ urlpatterns = [
     path('gas/<str:pk>/update/', GasUpdateView.as_view(), name='update_gas'),
     path('gas/<str:pk>/delete', GasDeleteView.as_view(), name='delete_gas'),
 
+    path('bookings/user-bookings/', UserBookingListView.as_view(), name='user-bookings'),
+    path('bookings/status/<str:bookingstatus>/', GetBookingStatusListView.as_view(), name='booking-status'),
     path('bookings/', BookingListView.as_view(), name='booking-list'),
     path('bookings/preview-booking/', BookingPreviewView.as_view(), name='preview-bookin'),
     path('bookings/create/', BookingCreateView.as_view(), name='booking-create'),
--- TripNitor_BE/TN_Api/views/__init__.py
+++ TripNitor_BE/TN_Api/views/__init__.py
@@ -1,6 +1,7 @@
-from .user_view import (RegisterView, LoginView, LogoutView)
+from .user_view import RegisterView, LoginView, LogoutView, UserDetailView
 from .driver_view import DriverCreateView, DriverListView, DriverDetailView, DriverUpdateView, DriverDeleteView
 from .van_view import VanCreateView, VanListView, VanDetailView, VanUpdateView, VanDeleteView
 from .gas_view import GasCreateView, GasListView, GasDetailView, GasUpdateView, GasDeleteView
 from .package_view import PackageCreateView, PackageListView, PackageDetailView, PackageUpdateView, PackageDeleteView
-from .booking_view import BookingListView, BookingCreateView, BookingDetailView, BookingUpdateView, BookingDeleteView, BookingPreviewView
+from .booking_view import (BookingListView, BookingCreateView, BookingDetailView, BookingUpdateView, BookingDeleteView, 
+BookingPreviewView, UserBookingListView, GetBookingStatusListView)
