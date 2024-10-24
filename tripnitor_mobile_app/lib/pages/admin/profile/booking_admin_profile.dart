c66053338dd9e07c87723f81b890514de4d173f6
Daweee <49843513+Daweee@users.noreply.github.com>
2024-10-24 23:15:40 +0800

    added cancel and confirm button, and minor ui changes

--- tripnitor_mobile_app/lib/pages/admin/profile/booking_admin_profile.dart
+++ tripnitor_mobile_app/lib/pages/admin/profile/booking_admin_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:tripnitor_mobile_app/constants/constant.dart';
 import '../../../../models/booking_model.dart';
@@ -23,13 +24,13 @@ class _BookingAdminProfileState extends State<BookingAdminProfile> {
             TextButton(
               child: Text('No'),
               onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop();
               },
             ),
             TextButton(
               child: Text('Yes'),
               onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop();
                 // Add your cancel logic here
                 print("Booking cancelled");
               },
@@ -42,16 +43,22 @@ class _BookingAdminProfileState extends State<BookingAdminProfile> {
 
   @override
   Widget build(BuildContext context) {
+    final DateFormat dateTimeFormat = DateFormat('d MMM yyyy, h:mm a');
     bool isBookingPending = widget.booking.status.toUpperCase() == 'PENDING';
 
     return Scaffold(
       backgroundColor: Color(ColorConstants.BACKGROUND_COLOR),
       appBar: AppBar(
         backgroundColor: Color(ColorConstants.BACKGROUND_COLOR),
-        title: Padding(
-          padding: const EdgeInsets.only(left: 100.0),
-          child: Text('Task'),
+        scrolledUnderElevation: 0.0,
+        title: Text(
+          dateTimeFormat.format(widget.booking.createdAt),
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -95,42 +102,6 @@ class _BookingAdminProfileState extends State<BookingAdminProfile> {
                           color: Colors.black, fontWeight: FontWeight.bold)),
                 ],
               ),
-              Divider(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Starting Location: "),
-                  Text(widget.booking.package.startLocation.name,
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Final Location: "),
-                  Text(widget.booking.package.finalDestination.name,
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Booking Date:"),
-                  Text(
-                      DateFormat('MMMM dd yyyy')
-                          .format(widget.booking.createdAt),
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
               SizedBox(
                 height: 10,
               ),
@@ -146,135 +117,231 @@ class _BookingAdminProfileState extends State<BookingAdminProfile> {
               SizedBox(
                 height: 10,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Total Distance: "),
-                  Text("${widget.booking.package.totalDistance} km",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Divider(),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Base Fare: "),
-                  Text('₱${widget.booking.baseFare}',
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Package Fare: "),
-                  Text('₱${widget.booking.package.basePrice}',
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Booking Duration: "),
-                  Text("(${widget.booking.numberOfNights}) ₱1000",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Divider(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Overall Total Price: "),
-                  Text(widget.booking.totalPrice,
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
-              ),
-              Divider(),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Payment Method: "),
-                  Text(widget.booking.modeOfPayment,
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
-                ],
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Package Details',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: FaIcon(
+                            FontAwesomeIcons.angleRight,
+                            color: Colors.black,
+                            size: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Starting Location: "),
+                        Text(widget.booking.package.startLocation.name,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Final Location: "),
+                        Text(widget.booking.package.finalDestination.name,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 height: 20,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .4,
-                    height: MediaQuery.of(context).size.height * .06,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(ColorConstants.PRIMARY_COLOR),
-                        disabledBackgroundColor:
-                            Color(ColorConstants.PRIMARY_COLOR)
-                                .withOpacity(0.5),
-                      ),
-                      onPressed: isBookingPending
-                          ? () {
-                              // Add your confirm logic here
-                              print("Booking confirmed");
-                            }
-                          : null,
-                      child: Text(
-                        isBookingPending ? 'Confirm' : 'Begin',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Payment Method: "),
+                        Text(widget.booking.modeOfPayment,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                      ],
                     ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .4,
-                    height: MediaQuery.of(context).size.height * .06,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(ColorConstants.ERROR_COLOR),
-                        disabledBackgroundColor: Colors.red.withOpacity(0.5),
-                      ),
-                      onPressed: isBookingPending
-                          ? () {
-                              _showCancelConfirmationDialog();
-                            }
-                          : null,
-                      child: Text(
-                        'Cancel',
-                        style: TextStyle(
-                          color: Colors.white,
+                    Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Total Price:"),
+                        Row(
+                          children: [
+                            Text(
+                              "₱${widget.booking.totalPrice}",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(width: 8),
+                            GestureDetector(
+                              onTap: () {},
+                              child: FaIcon(
+                                FontAwesomeIcons.angleRight,
+                                color: Colors.black,
+                                size: 16,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
+                      ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Driver Details',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: FaIcon(
+                            FontAwesomeIcons.angleRight,
+                            color: Colors.black,
+                            size: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children:
+                          widget.booking.drivers.asMap().entries.map((entry) {
+                        final index = entry.key;
+                        final driver = entry.value;
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4),
+                          child: Text(driver.user.name),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              if (widget.booking.status == 'PENDING') ...[
+                _confirmBookingButton(),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+              _cancelBookingButton(widget.booking.status),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cancelBookingButton(String bookingStatus) {
+    final bool isEnabled = bookingStatus == "PENDING";
+
+    return SizedBox(
+      width: double.infinity,
+      height: 45,
+      child: ElevatedButton(
+        onPressed: isEnabled ? () {} : null,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(ColorConstants.CANCEL_COLOR),
+          disabledBackgroundColor: Color(ColorConstants.DISABLED_COLOR),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        child: Text(
+          'Cancel Booking',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _confirmBookingButton() {
+    return SizedBox(
+      width: double.infinity,
+      height: 45,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(ColorConstants.PRIMARY_COLOR),
+          disabledBackgroundColor: Color(ColorConstants.DISABLED_COLOR),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        child: Text(
+          'Confirm Booking',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
           ),
         ),
       ),
