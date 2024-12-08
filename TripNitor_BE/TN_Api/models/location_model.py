076acd75bb05323689e5b1f161dbd2f14a33d365
Daweee <49843513+Daweee@users.noreply.github.com>
2024-12-08 23:12:03 +0800

    modified lat and long fields

--- TripNitor_BE/TN_Api/models/location_model.py
+++ TripNitor_BE/TN_Api/models/location_model.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=255)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=22, decimal_places=16)
+    longitude = models.DecimalField(max_digits=22, decimal_places=16)
 
     def __str__(self):
         return self.name
